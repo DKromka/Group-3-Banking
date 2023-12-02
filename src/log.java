4070dcc481f196425b79b84f2d1e9f1bcbd620bc
David Kromka <46538919+DKromka@users.noreply.github.com>
2023-12-01 17:26:16 -0800

    Cleaned up code

--- src/log.java
+++ src/log.java
@@ -7,63 +7,39 @@ public class log implements Serializable {
 	protected Date date;
 	protected String user;
 	protected String account;
-	//protected action enum;   do we need this? shouldn't we just use message enum?
+	protected MessageType action;
 	float amount;
 	
 	
 	//constructor to set variables
-	public log(Date date, String user, String account, float amount){
-		this.date = setDate(date);
-		this.user = setUser(user);
-		this.account = setAccount(account);
-		this.amount = setAmount(amount);
-		
+	public log(String user,MessageType action, float amount, Date date, String account){
+		this.user = user;
+		this.action = action;
+		this.amount = amount;
+		this.date = date;
+		this.account = account;	
 	}
-
-	
-
-	//setters - private or public?
-	public String setAccount(String ACCOUNT) {
-		return this.account = ACCOUNT;
-	}
-
-
-	public String setUser(String USER) {
-		
-		return this.user = USER;
-	}
-
-
-	public Date setDate(Date DATE) {
-		
-		return this.date = DATE;
-	}
-	
-	public float setAmount(float AMOUNT) {
-		return this.amount = AMOUNT;
-	}
-	 
 
 	//getters - public
-	public String getAccount(String ACCOUNT) {
-		return ACCOUNT;
-	}
-
-
-	public String getUser(String USER) {
-		
-		return USER;
-	}
-
-
-	public Date getDate(Date DATE) {
-		
-		return DATE;
+	
+	public String getUser() {	
+		return user;
 	}
 	
-	public Date getAmount(Date DATE) {
-			
-		return DATE;
+	public float getAmount() {	
+		return amount;
+	}
+	
+	public MessageType getAction() {
+		return action;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+	
+	public String getAccount() {
+		return account;
 	}
 	
 }
