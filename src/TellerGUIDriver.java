7e0fd1aa2361dfab8f8ed9f1207916fc86a6cc69
David Kromka <46538919+DKromka@users.noreply.github.com>
2023-12-02 19:54:51 -0800

    Fixed red squiggly

--- src/TellerGUIDriver.java
+++ src/TellerGUIDriver.java
@@ -1,8 +1,9 @@
+import java.awt.HeadlessException;
 import java.io.IOException;
 import java.net.UnknownHostException;
 
 public class TellerGUIDriver {
-	public static void main(String[] args) throws NumberFormatException, UnknownHostException, IOException {
+	public static void main(String[] args) throws NumberFormatException, UnknownHostException, IOException, HeadlessException, ClassNotFoundException {
 		TellerGUI the = new TellerGUI();
 		the.run();
 	}
