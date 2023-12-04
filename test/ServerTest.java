ba81f4292bf938695f5311945ea16d933513ab5a
Marc026 <mlapira@horizon.csueastbay.edu>
2023-12-03 17:26:09 -0800

    Updated ServerTest

--- test/ServerTest.java
+++ test/ServerTest.java
@@ -1,55 +1,50 @@
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import java.io.*;
-import java.net.Socket;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
 
 public class ServerTest {
 
-    private Server server;
+    private BankAccount testAccount;
+    private User testUser;
 
-    @BeforeEach
-    void setUp() {
-        // Initialize the server or set up common resources before each test.
-        server = new Server();
+    @Before
+    public void setUp() {
+        // Initialize necessary objects for testing
+        testUser = new User("TestUser", "TestPassword", false);
+        testAccount = new BankAccount("TestAccount", 1000.0f, 100.0f, AccountStatus.GOOD);
+        testAccount.addUser(testUser.getName());
     }
 
     @Test
-    void testVerifyLoginWithValidCredentials() {
-        // Create a mock socket with an InputStream and OutputStream.
-        try (ByteArrayOutputStream output = new ByteArrayOutputStream();
-             ObjectOutputStream objectOutputStream = new ObjectOutputStream(output);
-             ByteArrayInputStream input = new ByteArrayInputStream("LOGIN_REQ\nusername\npassword\n".getBytes());
-             ObjectInputStream objectInputStream = new ObjectInputStream(input);
-             Socket mockSocket = new Socket()) {
+    public void testDeposit() {
+        try {
+            float initialBalance = testAccount.getBalance();
+            float depositAmount = 200.0f;
 
-            // Mock the server to handle the incoming connection.
-            Server.ClientHandler clientHandler = server.createClientHandler(mockSocket);
-            new Thread(clientHandler).start();
+            // Simulate a deposit
+            testAccount.deposit(depositAmount);
 
-            // Set up the input stream for the server to read from
-            clientHandler.setInputStream(objectInputStream);
-
-            // Set up the output stream for the server to write to
-            clientHandler.setOutputStream(objectOutputStream);
-
-            // Run the server logic
-            clientHandler.verifyLogin();
-
-            // Assert that the server responds with a success message
-            Message response = (Message) objectInputStream.readObject();
-            assertEquals(MessageType.SUCCESS, response.getType());
-            assertEquals("Login Successful", response.getData());
-
-        } catch (IOException | ClassNotFoundException e) {
+            // Check if the balance has been updated correctly
+            assertEquals(initialBalance + depositAmount, testAccount.getBalance(), 0.01);
+        } catch (Exception e) {
             fail("Exception thrown: " + e.getMessage());
         }
     }
 
-    // Add more test methods for the Server class.
-
     @Test
-    void testHandleDepositWithValidAccount() {
-        
+    public void testWithdrawal() {
+        try {
+            float initialBalance = testAccount.getBalance();
+            float withdrawalAmount = 50.0f;
+
+            // Simulate a withdrawal
+            boolean success = testAccount.withdraw(withdrawalAmount);
+
+            // Check if the withdrawal was successful and the balance has been updated correctly
+            assertTrue(success);
+            assertEquals(initialBalance - withdrawalAmount, testAccount.getBalance(), 0.01);
+        } catch (Exception e) {
+            fail("Exception thrown: " + e.getMessage());
+        }
     }
 }
